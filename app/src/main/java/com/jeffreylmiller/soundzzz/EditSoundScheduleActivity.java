d4b0c13e43a9a25490211df2df70acef3ee868f3
Jeffrey Miller <Millerjl07@mounties.mansfield.edu>
2018-01-28 23:57:58 -0500

    Add Edit Sound Schedule Activity

---
+++ app/src/main/java/com/jeffreylmiller/soundzzz/EditSoundScheduleActivity.java
@@ -0,0 +1,13 @@
+package com.jeffreylmiller.soundzzz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditSoundScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_sound_schedule);
+    }
+}
